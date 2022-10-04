eda42547988e1f6648ab2ef2d54b34990c39b2b2
Abdul Hamid <lutifiatulaziz@gmail.com>
2022-10-04 11:35:41 +0700

    fix : user

--- repository/user/models.py
+++ repository/user/models.py
@@ -2,16 +2,13 @@ import datetime
 from typing import Optional
 
 from pydantic import validator, EmailStr
-from sqlmodel import SQLModel, Field, Relationship
+from sqlmodel import SQLModel, Field, Relationship, Column, String
 
 
 class User(SQLModel, table=True):
     # __tablename__ = 'ucok' #uncoment this to use different tablenames than lower classname
-    id: Optional[int] = Field(primary_key=True)
-    username: str = Field(index=True)
+    id: Optional[int] = Field(default=None, primary_key=True)  # primary key and auto increment
+    username: str = Field(index=True, sa_column=Column("username", String, unique=True))
     password: str = Field(max_length=256, min_length=6)
     email: EmailStr
     created_at: datetime.datetime = datetime.datetime.now()
-
-
-
